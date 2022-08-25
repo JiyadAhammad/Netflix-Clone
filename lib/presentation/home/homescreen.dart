0b27a4075e8395a61374379e6a0c2a3bd5d10a49
JiyadAhammad <jiyadahammad99@gmail.com>
2022-08-25 21:08:25 +0530

    home Screen UI completed

--- lib/presentation/home/homescreen.dart
+++ lib/presentation/home/homescreen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:netflix/constants/colors/colors.dart';
 import 'package:netflix/constants/constants_widgets.dart';
 import 'package:netflix/presentation/home/widgets/number_card_tile.dart';
 import '../widgets/main_title_card.dart';
+
+ValueNotifier scrollNotifier = ValueNotifier(true);
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,54 +13,140 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ListView(
-          children: [
-            Stack(
+      body: ValueListenableBuilder(
+        valueListenable: scrollNotifier,
+        builder: (BuildContext context, index, _) {
+          return NotificationListener<UserScrollNotification>(
+            onNotification: (notification) {
+              final ScrollDirection direction = notification.direction;
+              if (direction == ScrollDirection.reverse) {
+                scrollNotifier.value = false;
+              } else if (direction == ScrollDirection.forward) {
+                scrollNotifier.value = true;
+              }
+              return true;
+            },
+            child: Stack(
               children: [
-                Container(
-                  width: double.infinity,
-                  height: 600,
-                  // color: Colors.green,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('asset/image/peak.jpg'),
+                ListView(
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: 600,
+                          // color: Colors.green,
+                          decoration: const BoxDecoration(
+                            color: Colors.lightBlue,
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage('asset/image/peak.jpg'),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          left: 0,
+                          right: 0,
+                          bottom: 20,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              const IconsInHome(
+                                icon: Icons.add,
+                                buttonName: 'My List',
+                              ),
+                              _stackIconHomeScreen(),
+                              const IconsInHome(
+                                icon: Icons.info,
+                                buttonName: 'Info',
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
                     ),
-                  ),
+                    kheight,
+                    const MainCardTile(cardTitle: 'Released in the past year'),
+                    kheight,
+                    const MainCardTile(cardTitle: 'Trending Now'),
+                    kheight,
+                    const NumbeCard(),
+                    kheight,
+                    const MainCardTile(cardTitle: 'Tense Dramas'),
+                    kheight,
+                    const MainCardTile(cardTitle: 'South Indian Cinema'),
+                  ],
                 ),
-                Positioned(
-                  left: 0,
-                  right: 0,
-                  bottom: 20,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      const IconsInHome(
-                        icon: Icons.add,
-                        buttonName: 'My List',
-                      ),
-                      _stackIconHomeScreen(),
-                      const IconsInHome(
-                        icon: Icons.info,
-                        buttonName: 'Info',
-                      ),
-                    ],
-                  ),
-                )
+                scrollNotifier.value == true
+                    ? AnimatedContainer(
+                        duration: const Duration(
+                          milliseconds: 1000,
+                        ),
+                        width: double.infinity,
+                        height: 90,
+                        color: Colors.black.withOpacity(0.2),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'asset/image/netflix_PNG22.png',
+                                  width: 60,
+                                  height: 60,
+                                ),
+                                const Spacer(),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.cast,
+                                    color: kwhite,
+                                    size: 30,
+                                  ),
+                                ),
+                                // kwidth,
+                                Container(
+                                  height: 30,
+                                  width: 30,
+                                  color: Colors.lightBlue,
+                                ),
+                                kwidth,
+                              ],
+                            ),
+                            kheight,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: const [
+                                Text(
+                                  "Tv Shows",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  "Movies",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  "Categories",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    : kheight,
               ],
             ),
-            kheight,
-            const MainCardTile(cardTitle: 'Released in the past year'),
-            kheight,
-            const MainCardTile(cardTitle: 'Trending Now'),
-            kheight,
-            const NumbeCard(),
-            kheight,
-            const MainCardTile(cardTitle: 'Tense Dramas'),
-            kheight,
-            const MainCardTile(cardTitle: 'South Indian Cinema'),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
@@ -102,13 +191,13 @@ class IconsInHome extends StatelessWidget {
         Icon(
           icon,
           color: kwhite,
-          size: 35,
+          size: 30,
         ),
         Text(
           buttonName,
           style: const TextStyle(
             color: kwhite,
-            fontSize: 18,
+            fontSize: 16,
           ),
         )
       ],
